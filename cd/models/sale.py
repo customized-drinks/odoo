82369f9b3f5e018301412f4a716eb14a9e86cdeb
jdrsn <github@jaderson.com>
2021-08-30 00:33:47 +0200

    further improvements

--- cd/models/sale.py
+++ cd/models/sale.py
@@ -12,3 +12,10 @@ class SaleOrder(models.Model):
             # 'date_order': fields.Datetime.now()
         }
 
+    # def _find_mail_template(self, force_confirmation_template=False):
+    #     template_id = False
+    #
+    #     if self.state == 'draft':
+    #         template_id = self.env['ir.model.data'].xmlid_to_res_id('sale.mail_template_sale_confirmation', raise_if_not_found=False)
+    #
+    #     return template_id
