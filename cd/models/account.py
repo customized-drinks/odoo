8aeceb8504c9c942190fa017f5532695a87c1249
jdrsn <github@jaderson.com>
2021-09-29 12:30:05 +0200

    further improvements

--- cd/models/account.py
+++ cd/models/account.py
@@ -8,3 +8,14 @@ class AccountMove(models.Model):
 
     delivery_date = fields.Date(string='Delivery Date')
 
+    def _find_mail_template(self, force_confirmation_template=False):
+        template_id = False
+
+        if self.state == 'draft':
+            template_id = 26
+        elif self.state == 'sent':
+            template_id = 17
+        elif self.state == 'sale':
+            template_id = 18
+
+        return template_id
--- cd/models/sale.py
+++ cd/models/sale.py
@@ -19,13 +19,17 @@ class SaleOrder(models.Model):
             # 'date_order': fields.Datetime.now()
         }
 
-    # def _find_mail_template(self, force_confirmation_template=False):
-    #     template_id = False
-    #
-    #     if self.state == 'draft':
-    #         template_id = self.env['ir.model.data'].xmlid_to_res_id('sale.mail_template_sale_confirmation', raise_if_not_found=False)
-    #
-    #     return template_id
+    def _find_mail_template(self, force_confirmation_template=False):
+        template_id = False
+
+        if self.state == 'draft':
+            template_id = 26
+        elif self.state == 'sent':
+            template_id = 17
+        elif self.state == 'sale':
+            template_id = 18
+
+        return template_id
 
     def _prepare_invoice(self):
         self.ensure_one()
